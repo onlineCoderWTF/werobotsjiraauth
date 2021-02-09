258fc5b968e657002a6f709b2fdd277fc400b20b
Ortabaev Alan <a.ortabaev@ucs.ru>
2021-02-09 18:21:29 +0300

    #0 init

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using System.Collections.Specialized;   // Redmine.Net.Api
 using NDesk.Options;
 using System.Linq;
 using Atlassian.Jira;
+using System.Net;
+using MihaZupan;
+using OpenSSL;
 
 namespace werobotsjiraauth
 {
@@ -14,9 +17,14 @@ namespace werobotsjiraauth
         {
             string[] lines = System.IO.File.ReadAllLines(@"werobotsjiraauth.txt");
             //
-            var jiraIO = Jira.CreateOAuthRestClient(lines[0], lines[1], lines[2], lines[3], lines[4]);  // URL, consKey, consSec, authAcc, authSec
+            var privateKey = lines[2]; // <- get your private key in here
+            var decoder = new OpenSSL.PrivateKeyDecoder.OpenSSLPrivateKeyDecoder();
+            var keyInfo = decoder.Decode(privateKey);
+            var consumerSecret = OpenSSL.PrivateKeyDecoder.RSAExtensions.ToXmlString(keyInfo, true);
+
+            var jiraIO = Jira.CreateOAuthRestClient(lines[0], lines[1], consumerSecret, lines[3], lines[4]);  // URL, consKey, consSec, authAcc, authSec
             //
-            jiraIO.RestClient.Settings.EnableRequestTrace = true;
+            //jiraIO.RestClient.Settings.Proxy = new HttpToSocks5Proxy("127.0.0.1", 1080);
             //
             Atlassian.Jira.Issue moveTo = jiraIO.Issues.GetIssueAsync(lines[5]).Result;
             //
